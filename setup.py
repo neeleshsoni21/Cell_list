644eb06ba95af2eabc2fae4a0fdab67f67d5c3c4
neelesh <neeleshsoni03@gmail.com>
2019-11-14 00:02:45 +0530

    Updating

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2013 Neelesh Soni <neelesh.soni@alumni.iiserpune.ac.in>, <neelrocks4@gmail.com>
+Copyright (C) 2013 Neelesh Soni <neelesh.soni@alumni.iiserpune.ac.in>, <neeleshsoni03@gmail.com>
 This file is part of celllist.
 
 celllist is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -13,9 +13,9 @@ from setuptools import setup
 setup(name='cell_list_v4.3',
       version='4.3',
       description='Cell_list code to determine the distances between neighbors',
-      url='http://192.168.8.252:8080/Neelesh_cell_list',
+      url='',
       author='Neelesh Soni',
-      author_email='neelrocks4@gmail.com',
+      author_email='neeleshsoni03@gmail.com',
       license='LGPL',
       packages=['cell_list'],
       package_data={'cell_list': ['input/*.pdb','output/*.*',],},
